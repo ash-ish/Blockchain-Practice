d0419f7c88e9c837d360a3dfca4e196437480bfa
Ashish Shah1 <Ashish_shah1@epam.com>
2020-01-28 15:26:13 +0530

    basic blockchain with two blocks

---
+++ source code/main.js
@@ -0,0 +1,42 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "01/01/2020", "Genesis Block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+}
+
+let ashishCoin = new Blockchain();
+ashishCoin.addBlock(new Block(1, "28/01/2020", {amount:20}));
+ashishCoin.addBlock(new Block(2, "28/01/2020", {amount:10}));
+
+console.log(JSON.stringify(ashishCoin, null, 4));
