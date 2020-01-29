7ce7af7d04525ae99332ea02e49f62b3070cf150
Ashish Shah1 <Ashish_shah1@epam.com>
2020-01-29 18:14:13 +0530

    added transaction function

--- source code/main.js
+++ source code/main.js
@@ -1,17 +1,24 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block{
-    constructor(index, timestamp, data, previousHash = ''){
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = ''){
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 1;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash +this.nonce).toString();
+        return SHA256(this.timestamp + JSON.stringify(this.data) + this.previousHash +this.nonce).toString();
     }
 
     mineBlock(difficulty){
